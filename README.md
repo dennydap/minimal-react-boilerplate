836096fe3b80097152d06d052da63f1d82304abe
Denny Pradipta <denny.aditya.p@gmail.com>
2019-04-30 13:53:46 +0700

    [Update] Added SASS Loader, Bulma, and major changes to plugins used

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minimal React Boilerplate
+
+Minimal React Boilerplate is a React Boilerplate without Create React App. It includes Bulma for the UI Framework.
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
-module.exports = {
+var config = Object.assign({}, config, {
   entry: "./src/index.js",
   module: {
     rules: [
@@ -10,7 +12,7 @@ module.exports = {
         use: ["babel-loader"]
       },
       {
-        test: /\.scss$/,
+        test: /\.(scss|sass)$/,
         use: [
           {
             loader: "style-loader"
@@ -22,20 +24,39 @@ module.exports = {
             loader: "sass-loader"
           }
         ]
+      },
+      {
+        test: /\.(woff2?|jpe?g|png|gif|ico|svg)$/,
+        use: "file-loader?name=./images/[name].[ext]"
       }
     ]
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"]
+    extensions: ["*", ".js", ".jsx"],
+    modules: ["node_modules", "src"],
+    alias: {
+      "_variables.sass": path.resolve(__dirname, "./src/styles/_variables.sass")
+    }
   },
   output: {
     path: __dirname + "/dist",
     publicPath: "/",
     filename: "bundle.js"
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, "./src/styles/_style.css"),
+        to: path.resolve(__dirname, "./dist/_style.css")
+      }
+    ])
+  ],
   devServer: {
     contentBase: "./dist",
-    hot: true
+    hot: true,
+    port: 3001
   }
-};
+});
+
+module.exports = config;
